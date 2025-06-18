17645d1f59d72c14151ac19c58edba29c7ccce2a
akash <akashgit14@gmail.com>
2025-06-18 23:50:55 +0530

    print butterfly pattern

---
+++ pattern18.js
@@ -0,0 +1,19 @@
+let totalRows = 5;
+totalRows = 2 * totalRows - 1;
+const totalColums = totalRows + 1;
+let k = 0;
+const halfRows = Math.floor((totalRows +1) / 2 );
+console.log(totalRows, totalColums, halfRows)
+for(let i = 1; i <= totalRows; i++) {
+  let str = '';
+      if(i <= halfRows) k++;
+  if(i > halfRows) k--;
+  for(let j = 1; j<= totalColums; j++ ) {
+    if(j <= i && j >= totalColums + 1 - k) {
+      str+='*'
+    } else {
+      str+= ' ';
+    }
+  }
+   console.log(str)
+}
