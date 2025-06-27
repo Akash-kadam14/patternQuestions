2224bb403f45d6b6f760ed800298905e947cbe0e
akash <akashgit14@gmail.com>
2025-06-27 18:20:32 +0530

    printed pyramid star

--- pattern8.js
+++ pattern8.js
@@ -1,23 +1,23 @@
 /*
- 
-****
-***
-**
-*
+          *
+        * * *
+      * * * * *
+    * * * * * * *
+  * * * * * * * * *
   
-  
- */
+*/
 
-// const n = 5;
-// let str=''; 
-// for(let i = 1; i <= n; i++) {
-//     for(let j = 1; j <= n; j++) {
-//         if( j <= n + 1 - i){
-//             str += '*'
-//         } else {
-//             str += " ";
-//         }
-//     }
-//     str += "\n";
-// }
-// console.log(str);
+const row = 5
+const column = row * 2 - 1;
+
+for(let i = 1; i <= row; i++) {
+    let str = '';
+    for(let j = 1; j<= column; j++) {
+        if(j>= row+ 1 - i && j<= row - 1 + i){
+            str+= '*'
+        } else {
+            str+= ' ';
+        }
+    }
+    console.log(str);
+}
