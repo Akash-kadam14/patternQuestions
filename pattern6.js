592e75ae485621c0e454b31e1faadc68ce42c8d7
akash <akashgit14@gmail.com>
2025-06-27 18:10:15 +0530

    inverted right angle triangle print num from 1 to 5

--- pattern6.js
+++ pattern6.js
@@ -1,39 +1,19 @@
-/**
-         *
-        **
-       ***
-      ****
- */
-    //   let rows = 5;
+/*
 
-    //   // pattern variable carries the final pattern in string format
-    //   let pattern = "";
-      
-    //   // outer loop runs for `rows` no. of times
-    //   for (let n = 1; n <= rows; n++) {
-    //      // Inner loop - I -> prints spaces
-    //      for (let space = 1; space <= rows - n; space++) {
-    //         pattern += " ";
-    //      }
-      
-    //      // Inner Loop - II -> prints stars
-    //      for (let num = 1; num <= n; num++) {
-    //         pattern += "*";
-    //      }
-    //      pattern += "\n";
-    //   }
-    //   console.log(pattern);
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2 
+1
 
-const n = 5;
-let str=''; 
-for(let i = 1; i <= n; i++) {
-    for(let j = 0; j <= n; j++) {
-        if( j >= n + 1 - i){
-            str += '*'
-        } else {
-            str += " ";
-        }
+*/
+
+
+const row = 5;
+for(let i = 1; i <= row; i++) {
+    let str= ''
+    for(let j = 1; j <= row + 1 - i; j++) {
+        str+= j;
     }
-    str += "\n";
+    console.log(str);
 }
-console.log(str);
