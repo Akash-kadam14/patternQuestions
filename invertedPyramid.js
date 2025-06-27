e3adfe89cdf6e3b34aba3932664d99134bd6074c
akash <akashgit14@gmail.com>
2025-06-27 18:33:08 +0530

    printed inverted pyramid star

---
+++ invertedPyramid.js
@@ -0,0 +1,23 @@
+/*
+    * * * * * * * * *
+      * * * * * * *
+       * * * * *
+         * * *
+           *   
+*/
+
+
+const row = 50;
+const columns = 2 * row - 1;
+
+for(let i = 1; i<= row; i++) {
+    let str = '';
+    for(let j = 1; j<= columns; j++) {
+        if(j >= i && j<= columns + 1 - i) {
+            str+= '*'
+        } else {
+            str+= ' ';
+        }
+    }
+    console.log(str);
+}
