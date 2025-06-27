d40ae7b33235f2fc1e904fec17dee7da4c4960d9
akash <akashgit14@gmail.com>
2025-06-27 09:28:28 +0530

    right angle triangle star or numbers

--- pattern2.js
+++ pattern2.js
@@ -7,13 +7,20 @@ row
     * * *
     * * * *
     * * * * *
+    
+1
+12
+123
+1234
+12345
+
 */
 const n = 5;
 for(let i = 0; i <= n; i++) {
     let str='';
     for(let j = 1; j <= i ; j++) {
-        // str += '*'
-        str += j
+        str += '*'
+        // str += j
     }
     console.log(str)
 }
