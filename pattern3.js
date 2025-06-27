d950541979229be231d9926e536858029e54d5d5
akash <akashgit14@gmail.com>
2025-06-27 09:32:06 +0530

    right angle triangle numbers

--- pattern3.js
+++ pattern3.js
@@ -1,17 +1,19 @@
 /*
 
-****
-****
-****
-****
+1
+12
+123
+1234
+12345
  print the a4bove pattern
 */
 
-const n = 4;
-for(let row = 1; row <= n; row++) {
-    let str = '';
-    for(let col = 1; col <= n; col++) {
-        str += '*'
+
+const n = 5;
+for(let i = 0; i <= n; i++) {
+    let str='';
+    for(let j = 1; j <= i ; j++) {
+        str += j
     }
-    console.log(str);
+    console.log(str)
 }
