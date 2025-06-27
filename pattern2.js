8c90b942cd36035591840822f5cb6885fd71eb85
akash <akashgit14@gmail.com>
2025-06-27 09:26:54 +0530

    right angle triangle

--- pattern2.js
+++ pattern2.js
@@ -2,31 +2,20 @@
 
       column
 row   
-      * * * * *
-      * * * *
-      * * *
-      * * 
-      *
+    *
+    * *
+    * * *
+    * * * *
+    * * * * *
 */
 const n = 5;
 for(let i = 0; i <= n; i++) {
     let str='';
-    for(let j = 1; j <= n - i ; j++) {
+    for(let j = 1; j <= i ; j++) {
         // str += '*'
         str += j
     }
     console.log(str)
 }
-function printPattern(rows) {
-    for (let i = rows; i >= 1; i--) {
-        let str = "";
-        for (let j = rows; j >= rows - i + 1; j--) {
-            str += j;
-        }
-        console.log(str);
-    }
-}
-
-printPattern(5);
 
 
