397b6ee2d896e8b53c461dbdfe72fe1057c277ae
akash <akashgit14@gmail.com>
2025-07-02 19:48:42 +0530

    arrow start pattern

---
+++ arrowPattetrn.js
@@ -0,0 +1,31 @@
+/**
+  *
+  * *
+  * * *
+  * * * *
+  * * * * *
+  * * * *
+  * * *
+  * *
+  * 
+  
+ */
+
+const row = 10 
+const halfRow = Math.floor((row + 1) / 2); 
+console.log(halfRow);
+let k = 0;
+for(let i = 1; i <= 2 * row - 1; i++) {
+    let str = ''
+    if( i <= halfRow) k++;
+    if(i > halfRow) k--;
+    for(let j = 1; j <= halfRow; j++) {
+        if(j<=k) {
+            str+= '*'
+        } else {
+            str+= ' '
+        }
+    }
+    console.log(str);
+}
+
