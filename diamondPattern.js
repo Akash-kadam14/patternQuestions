d24892855a2718e869c45beb6e2bf0917017dde0
akash <akashgit14@gmail.com>
2025-07-02 19:44:50 +0530

    diamond start pattern changes done

--- diamondPattern.js
+++ diamondPattern.js
@@ -19,7 +19,7 @@ console.log(halfRow);
 let k = 0;
 for(let i = 1; i <= row; i++) {
     let str = '';
-    if(i < halfRow) k++;
+    if(i <= halfRow) k++;
     if(i > halfRow) k--;
     for(let j = 1; j<= column; j++) {
         if(j>= halfRow + 1 - k && j<= halfRow - 1 + k){
