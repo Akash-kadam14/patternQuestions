5d87ff6b690f05e1ea1152eaa68468f6c7dd6627
akash <akashgit14@gmail.com>
2025-07-04 19:41:44 +0530

    print numbered right angle with in increment order

--- pattern11.js
+++ pattern11.js
@@ -7,14 +7,14 @@
 7 8 9 10
 
 */
-const totalRows = 4;
+const totalRows = 5;
 
 let k = 1
 for(let i = 1; i <= totalRows; i++){
  let str= '';
  for(j = 1; j <= i; j++){
-    str+= k
-    k+=1       
+    str+= k + ' '
+    k++       
    }
    console.log(str);
  }
