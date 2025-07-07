238ff453a84a5292e6d076f241a12777e9861418
akash <akashgit14@gmail.com>
2025-07-07 19:46:09 +0530

    char ascii num pyramid pattern revise

--- pattern15.js
+++ pattern15.js
@@ -10,22 +10,16 @@ const totalColums = 2 * totalRows - 1;
 const halfColums = Math.ceil(totalColums / 2);
 
 for(let i = 1; i <= totalRows; i++) {
-  let str='';
+  let str = '';
   let charAsciiNum = 64;
-  for(let j = 1; j<= totalColums; j++) {
-    if(j>= halfColums + 1 - i && j<= halfColums - 1 + i) {
-        if(j<= halfColums) {
-          charAsciiNum++
-        str+= String.fromCharCode(charAsciiNum);
-    } else if(j> halfColums) {
-      --charAsciiNum
-       str+= String.fromCharCode(charAsciiNum);
-       ;
-    }
-    }
-    else {
-      str+= ' ';
+  for(let j = 1; j <= totalColums; j++) {
+    if(j >= halfColums + 1 - i && j<= halfColums - 1 + i) {
+      if( j<= halfColums) charAsciiNum++;
+      if( j> halfColums ) charAsciiNum--;
+      str+= String.fromCharCode(charAsciiNum)
+    } else {
+      str+= ' '
     }
   }
-  console.log(str)
+  console.log(str);
 }
