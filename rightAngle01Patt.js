0ebb162d1015005869edd8520534644438cf249e
akash <akashgit14@gmail.com>
2025-07-07 16:53:04 +0530

    remove console

--- rightAngle01Patt.js
+++ rightAngle01Patt.js
@@ -10,7 +10,6 @@
 let start = 1;
 for(let i = 1; i<= 5; i++) {
     let str = ''
-    console.log( i, 'i % 2 === 0', i % 2 === 0,);
     if( i % 2 === 0) start = 0;
     else start = 1;
     for(let j = 1; j <= i; j++) {
