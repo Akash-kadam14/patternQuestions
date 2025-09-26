3600f4338433a89866c9810fc01c551474dfd58a
akash <akashgit14@gmail.com>
2025-09-26 13:37:02 +0530

    arrow pattern changes done

--- arrowPattetrn.js
+++ arrowPattetrn.js
@@ -15,7 +15,7 @@ const row = 10
 const halfRow = Math.floor((row + 1) / 2); 
 console.log(halfRow);
 let k = 0;
-for(let i = 1; i <= 2 * row - 1; i++) {
+for(let i = 1; i <= row - 1; i++) {
     let str = ''
     if( i <= halfRow) k++;
     if(i > halfRow) k--;
