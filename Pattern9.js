c356ec18388b99a3e971492e7b4e36fbbfb6c006
akash <akashgit14@gmail.com>
2026-05-14 12:38:55 +0530

    left angle downside pattern.js

--- Pattern9.js
+++ Pattern9.js
@@ -9,18 +9,36 @@
 
 const totalRows = 5;
 
+// for (let i = 1; i <= totalRows; i++) {
+//   let str = "";
+
+//   for (let j = 1; j <= totalRows; j++) {
+//     if (j <= totalRows - i) {
+//       row += "  "; // print space (2 spaces for alignment)
+//     } else {
+//       // Calculate descending numbers starting from i down to 1
+//       // For j = totalRows - i + 1 → print i
+//       // For j = totalRows → print 1
+//       str += (totalRows - j + 1) + " ";
+//     }
+//   }
+//   console.log(str.trimEnd());
+// }
+
+
+const col = 5
 for (let i = 1; i <= totalRows; i++) {
   let str = "";
-
+  let k = col-totalRows+i;
   for (let j = 1; j <= totalRows; j++) {
-    if (j <= totalRows - i) {
-      row += "  "; // print space (2 spaces for alignment)
-    } else {
-      // Calculate descending numbers starting from i down to 1
-      // For j = totalRows - i + 1 → print i
-      // For j = totalRows → print 1
-      str += (totalRows - j + 1) + " ";
+    if(j>=totalRows+1-i && j<= totalRows -1+i) {
+      str+=k
+      k--;
+    }
+    else {
+      str+=' '
     }
   }
   console.log(str.trimEnd());
 }
+
---
+++ leftAngleDownword.js
@@ -0,0 +1,25 @@
+
+/*
+
+*****
+ ****
+  ***
+   **
+    *
+
+*/
+
+
+const row = 5;
+for(let i = 1; i <= row; i++) {
+    let str = ''
+    for(let j = 1; j<= row; j++) {
+        if(j>=i) {
+            str+='*'
+        }
+         else {
+            str+= ' '
+         }
+    }
+    console.log(str);
+}
